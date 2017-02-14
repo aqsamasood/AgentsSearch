27e2a98cf1d8480cc401e4e6795650737e8593a4
aqsa-fueled <aqsa@fueled.com>
2017-02-14 20:22:12 +0530

    refactor(index): refactor index to show search page

--- index.ios.js
+++ index.ios.js
@@ -1,54 +1,18 @@
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  NavigatorIOS
-} from 'react-native';
+import { AppRegistry, NavigatorIOS, StyleSheet } from 'react-native'
+import Search from './app/components/Search'
 
-var SearchPage = require('./app/UIComponents/Search');
-
-class Welcome extends Component {
-	render() {
-		return(
-			<View style = {{width: 100, height: 100, paddingTop: 100, backgroundColor: 'green'}}>
-			<Text> Hello World, Aqsa </Text>
-			</View>
-		);
-	}
-}
-export default class AgentsSearch extends Component {
+class AgentsSearch extends Component {
   render() {
-   return (
+    return (
       <NavigatorIOS
         initialRoute={{
-          component: SearchPage,
-          title: 'Real Estate Agents Finder',
+          component: Search,
+          title: 'Search Agents',
         }}
         style={{flex: 1}} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('AgentsSearch', () => AgentsSearch);
